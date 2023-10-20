54a443ecda563e67fb6e42c8a43dcdad1a1c45eb
oguzkadir <oguz.kadir@ankasource.com>
2023-10-20 16:03:14 +0300

    Add project files.

---
+++ TeamCityTest.Api/Controllers/TeamCityTestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamCityTest.Api.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TeamCityTestController : ControllerBase
+    {
+        public TeamCityTestController()
+        {
+             
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Ping()
+        {
+            return Ok("Api working on Docker!");
+        }
+
+    }
+}
